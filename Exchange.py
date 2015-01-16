856bfa0d92d405ca84a88f9b492b2b9b502cdb6c
Naoto Ida <n_ida@cgate.jp>
2015-01-16 21:22:24 +0900

    Made responses shorter, added validation

--- Exchange.py
+++ Exchange.py
@@ -14,48 +14,54 @@ SECOND_CURR_ACTUAL = ""
 def handle(text, mic, profile):
     def setFirstCurrency(text):
         global FIRST_CURR_ACTUAL
-        FIRST_CURR_ACTUAL = "Dollars"
+        FIRST_CURR_ACTUAL = text
+
 
     def setSecondCurrency(text):
         global SECOND_CURR_ACTUAL
-        SECOND_CURR_ACTUAL = "Yen"
+        SECOND_CURR_ACTUAL = text
+
 
     def convertToCode():
         global FIRST_CURR
         global FIRST_CURR_ACTUAL
         global SECOND_CURR
         global SECOND_CURR_ACTUAL
-        code_list = {"Yen" : "JPY", "Dollars" : "USD"}
+        code_list = {"YEN" : "JPY", "DOLLARS" : "USD"}
 
         for key, value in code_list.iteritems():
-            if key == FIRST_CURR_ACTUAL:
-                FIRST_CURR = value
-            if key == SECOND_CURR_ACTUAL:
-                SECOND_CURR = value
+                if key == FIRST_CURR_ACTUAL:
+                        FIRST_CURR = value
+                    if key == SECOND_CURR_ACTUAL:
+                        SECOND_CURR = value
 
-    mic.say("What is your first currency?")
+        if FIRST_CURR != "" and SECOND_CURR != "":
+            return True
+        else:
+            return False
+
+
+    mic.say("First currency?")
     setFirstCurrency(mic.activeListen())
 
-    mic.say("What is your second currency?")
+    mic.say("Second currency?")
     setSecondCurrency(mic.activeListen())
 
-    convertToCode()
+    if convertToCode():
+        mic.say("Getting exchange rate of " + FIRST_CURR_ACTUAL + " against " + SECOND_CURR_ACTUAL + ".")
+        jsonurl = urlopen(API_URL + "from=" + FIRST_CURR + "&to=" + SECOND_CURR)
 
-    #needs to make sure FIRST_CURR and SECOND_CURR are set
-
-	mic.say("Getting exchange rate of " + FIRST_CURR + " against " + SECOND_CURR + ".")
-	jsonurl = urlopen(API_URL + "from=" + FIRST_CURR + "&to=" + SECOND_CURR)
-
-	try:
-		rate = json.loads(jsonurl.read())
-	except ValueError, e:
-		pass # invalid json
-        mic.say("An error occured. Maybe the API is offline?")
+        try:
+            rate = json.loads(jsonurl.read())
+        except ValueError, e:
+            pass # invalid json
+            mic.say("An error occured. Maybe the API is offline?")
+        else:
+            pass # valid json
+            mic.say("Okay, here is the exchange rate.")
+            mic.say("It is approximately " + str(round(rate["rate"], -1)) + " " + SECOND_CURR_ACTUAL + " for 1 " + FIRST_CURR_ACTUAL + ".")
     else:
-		pass # valid json
-		mic.say("Okay, here is the exchange rate.")
-		mic.say("It is " + str(rate["rate"]) + " " + SECOND_CURR + " for 1 " + FIRST_CURR + ".")
-
+        mic.say("One or both currencies are not understood. Please try again.")
 
 def isValid(text):
-        return bool(re.search(r'\bcurrency|exchange\b', text, re.IGNORECASE))
+    return bool(re.search(r'\bcurrency|exchange\b', text, re.IGNORECASE))
